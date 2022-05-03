f1040ae4ff088dc194e1d8b59784030b5fadab55
bennaaym <mr.aymen.bennabi@gmail.com>
2022-05-04 02:13:18 +0300

    bennaaym/dashboard: hide different sections till the user setup a mood

--- apps/mobile/src/app/screens/Dashboard/index.tsx
+++ apps/mobile/src/app/screens/Dashboard/index.tsx
@@ -7,7 +7,7 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Dispatch } from 'redux';
 import styles from '../../../styles/DashboardStyles';
 import { useAccessToken } from '../../customHooks/useAuthToken';
@@ -29,34 +29,41 @@ const Dashboard = () => {
     deleteUserData();
     dispatch(setIsLoggedIn(false));
   };
+  const { mood } = useSelector((state: AppState) => state.challengeReducer);
 
   return (
-    <FlatList
-      style={styles.root}
-      data={[null]}
-      renderItem={() => {
-        return (
-          <View>
-            <StatusBar
-              translucent={true}
-              backgroundColor={'transparent'}
-              barStyle="dark-content"
-            />
-            <Header />
-            <View style={styles.screenContent}>
-              <GreetingBox />
-              <Appointment />
-              <Notifications />
-              <Categories />
-              <Blogs />
+    <View style={styles.root}>
+      <FlatList
+        contentContainerStyle={{ paddingBottom: 5 }}
+        data={[null]}
+        renderItem={() => {
+          return (
+            <View>
+              <StatusBar
+                translucent={true}
+                backgroundColor={'transparent'}
+                barStyle="dark-content"
+              />
+              <Header />
+              <View style={styles.screenContent}>
+                <GreetingBox />
+                {mood && (
+                  <>
+                    <Appointment />
+                    <Notifications />
+                    <Categories />
+                    <Blogs />
+                  </>
+                )}
+              </View>
+              {/* <TouchableOpacity onPress={logout}>
+             <Text>Logout</Text>
+           </TouchableOpacity> */}
             </View>
-            {/* <TouchableOpacity onPress={logout}>
-           <Text>Logout</Text>
-         </TouchableOpacity> */}
-          </View>
-        );
-      }}
-    />
+          );
+        }}
+      />
+    </View>
   );
 };
 
