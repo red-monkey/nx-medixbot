844d15790eb384f2180f5ac1289ef7d6f545e5ee
Djibril <idjibrilmaman@gmail.com>
2022-05-04 23:02:10 +0300

    🚧 image recognition

--- apps/api/src/app/controllers/foodRecognition.controller.ts
+++ apps/api/src/app/controllers/foodRecognition.controller.ts
@@ -1,11 +1,37 @@
 import * as fs from 'fs';
+import { finished } from 'stream/promises';
 import { IContext } from '../types';
 
-async function predictImage(input: { image: unknown }, ctx: IContext) {
-  const imageAsBase64 = fs.readFileSync('./your-image.png', 'base64');
-  console.log(input, imageAsBase64, ctx);
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+async function predictImage(input: { image: any }, ctx: IContext) {
+  const { createReadStream, filename } = await input.image;
 
-  return {};
+  const filePath = `apps/api/src/uploads/${filename}`;
+
+  const stream = createReadStream();
+  const out = fs.createWriteStream(filePath);
+  stream.pipe(out);
+  await finished(out);
+
+  const imageAsBase64 = fs.readFileSync(filePath, 'base64');
+
+  fs.unlink(filePath, (err) => {
+    console.log(err ? err : 'file deleted');
+  });
+  console.log(imageAsBase64.substring(0, 100));
+
+  /**
+   * Send to image to the api for prediction
+   */
+
+  return {
+    accuracy: 95.8,
+    foodName: 'Smit',
+    nutrients: {
+      protein: 20,
+      fat: 50,
+    },
+  };
 }
 
 export default {
