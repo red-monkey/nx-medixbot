d47e78fcc519a820f18fba069b448c8c1111ee04
bennaaym <mr.aymen.bennabi@gmail.com>
2022-05-07 00:35:51 +0300

    update

--- apps/mobile/src/app/App.tsx
+++ apps/mobile/src/app/App.tsx
@@ -15,7 +15,7 @@ const App = () => {
   }, []);
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      {isLoggedIn ? <LoggedInUserAppContainer /> : <AppContainer />}
+      {true ? <LoggedInUserAppContainer /> : <AppContainer />}
     </SafeAreaView>
   );
 };
